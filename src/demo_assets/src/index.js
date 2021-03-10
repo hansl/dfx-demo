7f607ac0a4204daf9632fe1c0733615bdb8c000b
Hans Larsen <hans@larsen.online>
2021-03-10 11:20:32 -0800

    Initial commit.

---
+++ src/demo_assets/src/index.js
@@ -0,0 +1,12 @@
+import { Actor, HttpAgent } from '@dfinity/agent';
+import { IDL as demo_idl, canisterId as demo_id } from '@ic/canisters/demo';
+
+const agent = new HttpAgent();
+const demo = Actor.createActor(demo_idl, { agent, canisterId: demo_id })
+
+document.getElementById("clickMeBtn").addEventListener("click", async () => {
+  const name = document.getElementById("name").value.toString();
+  const greeting = await demo.greet(name);
+
+  document.getElementById("greeting").innerText = greeting;
+});
